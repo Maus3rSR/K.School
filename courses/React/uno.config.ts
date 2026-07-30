a3d02c567dc60013e07fc97dbccb0da0a1f890ab
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2026-07-30 10:24:32 +0200

    feat(react): ajoute icônes VSCode pour gestionnaires de paquets npm/pnpm
    Ajoute @iconify-json/vscode-icons en devDependency et configure uno.config.ts avec safelist pour les icônes npm et pnpm (versions light et standard).

---
+++ courses/React/uno.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'unocss'
+
+export default defineConfig({
+  safelist: [
+    'i-vscode-icons:file-type-light-pnpm',
+    'i-vscode-icons:file-type-pnpm',
+    'i-vscode-icons:file-type-npm',
+    'i-vscode-icons:file-type-light-npm',
+  ],
+})
